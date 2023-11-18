f8ff8dce612e4a15766c7fc75b0a8a17bbee0d2c
Kayode <babalolakayode44@gmail.com>
2023-11-18 15:52:05 +0100

    recenter and emoji change

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -4,15 +4,17 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="h-[60vh] w-4/5 bg-neutral-800 m-auto rounded-lg p-10 flex flex-col items-center justify-center">
-        <p className="text-lg text-center">
-          <TypeAnimation
-            sequence={[
-              "Soooo, this idea came to me on a very strange monday evening, on my way back home from work. I am quite poor, considering the economy and all that, so i got to thinking, *how can i appreciate everyone that beleives in me/ made an effort to look out for me/ made me happy(a very difficult task that some did with ease) *",
-            ]}
-            speed={40}
-          />
-        </p>
+      <div className="main-body flex flex-col items-center ">
+        <div className="h-[60vh] w-4/5 bg-neutral-800 m-auto rounded-lg p-10 flex flex-col items-center justify-center overflow-y-auto">
+          <p className="text-lg">
+            <TypeAnimation
+              sequence={[
+                "Soooo, this idea came to me on a very strange monday evening, on my way back home from work. I am quite poor, considering the economy and all that, so i got to thinking, *how can i appreciate everyone that beleives in me/ made an effort to look out for me/ made me happy(a very difficult task that some did with ease) *",
+              ]}
+              speed={40}
+            />
+          </p>
+        </div>
       </div>
     </>
   );
