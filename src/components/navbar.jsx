e0b0d178812a73b15e86addab6a0b6e56475c55a
Kayode <babalolakayode44@gmail.com>
2023-11-18 16:04:50 +0100

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="flex md:justify-between justify-around items-center h-16 px-16 ">
-      <p className="font-bold text-lg">With ❤ 🩹, Kay</p>
+      <p className="font-bold text-lg">With ❤️‍🩹, Kay</p>
 
       <ul className="justify-center gap-8 hidden md:flex">
         <li>Friends</li>
