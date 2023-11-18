126f4d3e3b2126a26191c4f2bcb45dc4f32e272a
Kayode <babalolakayode44@gmail.com>
2023-11-18 16:14:15 +0100

    minor fixes

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
     <>
       <Navbar />
       <div className="main-body flex flex-col items-center ">
-        <div className="h-[60vh] w-4/5 bg-neutral-800 m-auto rounded-lg p-10 flex flex-col items-center justify-center overflow-y-auto">
+        <div className="h-[60vh] w-4/5 bg-neutral-800 m-auto rounded-lg p-4 md:p-8 flex flex-col items-center justify-center overflow-y-auto">
           <p className="text-lg">
             <TypeAnimation
               sequence={[
-                "Soooo, this idea came to me on a very strange monday evening, on my way back home from work. I am quite poor, considering the economy and all that, so i got to thinking, *how can i appreciate everyone that beleives in me/ made an effort to look out for me/ made me happy(a very difficult task that some did with ease) *",
+                "Soooo, this idea came to me on a very strange monday evening, on my way back home from work. I am quite poor, considering the economy and all that, so i got to thinking",
               ]}
               speed={40}
             />
