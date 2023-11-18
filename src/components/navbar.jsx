c6ebf30a04cbb2970a7115773dd3cfd0911c791a
Kayode <babalolakayode44@gmail.com>
2023-11-18 15:40:04 +0100

    initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="flex md:justify-between justify-around items-center h-16 px-16 ">
+      <p className="font-bold text-lg">With 💕, Kay</p>
+
+      <ul className="justify-center gap-8 hidden md:flex">
+        <li>Friends</li>
+        <li>Family</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/views/start.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Start = () => {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center">
+      {" "}
+      <Link to="/home">
+        <p className="text-3xl font-bold animate-pulse hover:scale-125 duration-200">With 💕, Kay</p>
+      </Link>
+    </div>
+  );
+};
+
+export default Start;
